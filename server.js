de10ca5b7a3729c608bcd4a14d80d06ff8c9bdeb
topilski <atopilski@gmail.com>
2016-08-17 08:44:25 +0300

    Remove design

--- server.js
+++ server.js
@@ -36,9 +36,12 @@ var listener = io.listen(server);
 
 // settings
 app.locals.site = {
-    title: 'FastoNoSQL - cross-platform GUI Manager for Redis, Memcached and SSDB Databases',
+    title: 'FastoNoSQL',
     domain: 'http://46.101.179.225',
+    keyword: 'FastoNoSQL, GUI Manager, Redis gui, Memcached gui, SSDB gui',
     description: 'FastoNoSQL it is GUI platform for NoSQL databases.',
+    small_description: 'FastoNoSQL - cross-platform GUI Manager for Redis, Memcached and SSDB Databases.',
+    large_description: 'FastoNoSQL — is a cross-platform GUI Manager for Redis, Memcached and SSDB Databases(i.e. Admin GUI Client). Our Desktop Client works on the most amount of Linux systems, also on Windows, Mac OS X, FreeBSD and Android platforms.',
     public_directory: public_dir_abs_path,
     users_directory: public_downloads_users_dir_abs_path,
     google_analitics_token: settings_config.google_analitics_token,
