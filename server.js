5f1405d1b8f3e6e0c8c3b9d20fb81c10552d0a5a
topilski <atopilski@gmail.com>
2016-09-14 08:43:22 +0300

    Next typo

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: project.name_lowercase })); // session secret
+app.use(session({ secret: app.locals.project.name_lowercase })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
