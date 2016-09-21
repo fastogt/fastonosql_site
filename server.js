163051271c628ed6515b79da19609e220948622f
Alexandr <atopilski@gmail.com>
2016-09-21 13:16:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ listener.on('connection', function (socket) {
           socket.emit('status_rabbitmq', { 'email': in_json.email, 'progress': 0, 'message': 'Send request to build server' } ); //
           
           var rpc = new (require('./app/amqprpc'))(rabbit_connection);
-          var branding_variables = '-DUSER_SPECIFIC_ID=' + in_json.id + ' -DUSER_SPECIFIC_LOGIN=' + in_json.email + ' -DUSER_SPECIFIC_PASSWORD=' + in_json.password;
+          var branding_variables = '-DIS_PUBLIC_BUILD=OFF -DUSER_SPECIFIC_ID=' + in_json.id + ' -DUSER_SPECIFIC_LOGIN=' + in_json.email + ' -DUSER_SPECIFIC_PASSWORD=' + in_json.password;
           
           var request_data_json = {
               'branding_variables': branding_variables,
