a3b6daad6b9a705e60d277c64bb1b5530c0cedd0
topilski <atopilski@gmail.com>
2017-11-06 09:21:36 +0300

    Pass new

--- server.js
+++ server.js
@@ -253,7 +253,7 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport, nev); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
