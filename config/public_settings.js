d9d1fa82ff8c9aa23ac73eb7a6a15e37311f6e3a
topilski <atopilski@gmail.com>
2017-11-14 07:52:44 +0300

    Review donate

--- config/public_settings.js
+++ config/public_settings.js
@@ -23,6 +23,7 @@ module.exports = {
         twitter_name: 'FastoNoSQL',
         twitter_link: 'https://twitter.com/FastoNoSQL',
 
+        paypal_token: 'FY5JBFRE6NP9Y',
         google_analitics_token: 'UA-56403848-4',
         //data_ad_client: 'ca-pub-4299795201353238',
         //data_ad_slot: '1261996191',
