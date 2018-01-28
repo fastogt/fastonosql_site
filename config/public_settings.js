99ac4161082814db69cfc4c1115df81cc7e6f395
topilski <atopilski@gmail.com>
2018-01-28 08:15:22 +0300

    Pika

--- config/public_settings.js
+++ config/public_settings.js
@@ -10,9 +10,9 @@ module.exports = {
         name: 'FastoNoSQL',
         version: '1.0.0',
         domain: 'https://fastonosql.com',
-        keywords: 'fastonosql, nosql, gui, manager, redis, client, memcached, ssdb, leveldb, rocksdb, lmdb, unqlite, upscaledb, forestdb, admin, tool',
+        keywords: 'fastonosql, nosql, gui, manager, redis, client, memcached, ssdb, leveldb, rocksdb, lmdb, unqlite, upscaledb, forestdb, pika, admin, tool',
         description: 'FastoNoSQL it is GUI platform for NoSQL databases.',
-        small_description: 'FastoNoSQL - cross-platform GUI Manager for Redis, Memcached, SSDB, RocksDB, LMDB, UpscaleDB, Unqlite and ForestDB databases.',
+        small_description: 'FastoNoSQL - cross-platform GUI Manager for Redis, Memcached, SSDB, RocksDB, LMDB, UpscaleDB, Unqlite, ForestDB and Pika databases.',
         large_description: 'FastoNoSQL — is a cross-platform GUI Manager for Redis, Memcached, SSDB, RocksDB, LMDB, UpscaleDB, Unqlite and ForestDB databases(i.e. Admin GUI Client). Our Desktop Client works on the most amount of Linux systems, also on Windows, Mac OS X, FreeBSD and Android platforms.',
 
         github_link: 'https://github.com/fastogt/fastonosql',
@@ -27,15 +27,16 @@ module.exports = {
         //data_ad_client: 'ca-pub-4299795201353238',
         //data_ad_slot: '1261996191',
 
-        supported_databases: [{'name': 'Redis', 'option': 'BUILD_WITH_REDIS', 'active' : true},
-            {'name': 'Memcached', 'option': 'BUILD_WITH_MEMCACHED', 'active' : true},
-            {'name': 'SSDB', 'option': 'BUILD_WITH_SSDB', 'active' : true},
-            {'name': 'LevelDB', 'option': 'BUILD_WITH_LEVELDB', 'active' : true},
-            {'name': 'RocksDB', 'option': 'BUILD_WITH_ROCKSDB', 'active' : false},
-            {'name': 'LMDB', 'option': 'BUILD_WITH_LMDB', 'active' : true},
-            {'name': 'Unqlite', 'option': 'BUILD_WITH_UNQLITE', 'active' : true},
-            {'name': 'UpscaleDB', 'option': 'BUILD_WITH_UPSCALEDB', 'active' : false},
-            {'name': 'ForestDB', 'option': 'BUILD_WITH_FORESTDB', 'active' : false}]
+        supported_databases: [{'name': 'Redis', 'option': 'BUILD_WITH_REDIS', 'active': true},
+            {'name': 'Memcached', 'option': 'BUILD_WITH_MEMCACHED', 'active': true},
+            {'name': 'SSDB', 'option': 'BUILD_WITH_SSDB', 'active': true},
+            {'name': 'LevelDB', 'option': 'BUILD_WITH_LEVELDB', 'active': true},
+            {'name': 'RocksDB', 'option': 'BUILD_WITH_ROCKSDB', 'active': false},
+            {'name': 'LMDB', 'option': 'BUILD_WITH_LMDB', 'active': true},
+            {'name': 'Unqlite', 'option': 'BUILD_WITH_UNQLITE', 'active': true},
+            {'name': 'UpscaleDB', 'option': 'BUILD_WITH_UPSCALEDB', 'active': false},
+            {'name': 'ForestDB', 'option': 'BUILD_WITH_FORESTDB', 'active': false},
+            {'name': 'Pika', 'option': 'BUILD_WITH_PIKA', 'active': true}]
     },
 
     support: {
