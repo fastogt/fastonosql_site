cad8b04f78c4d2cc965e92ff8644a62a73255dfb
topilski <atopilski@gmail.com>
2018-03-11 09:57:55 +0300

    Review

--- app/routes.js
+++ app/routes.js
@@ -346,8 +346,9 @@ module.exports = function (app, passport, nev) {
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
+    if (req.isAuthenticated()) {
         return next();
+    }
 
     res.redirect('/');
 }
