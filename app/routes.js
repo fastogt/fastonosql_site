3c52cfa720d3d04c3948e6ace98246d410f0c125
imorozov <imdmz143@gmail.com>
2018-03-31 02:00:22 +0300

    Deleted user method rewrite.

--- app/routes.js
+++ app/routes.js
@@ -192,25 +192,14 @@ module.exports = function (app, passport, nev) {
         var user = req.user;
 
         if (user.email_subscription) {
-            mailerLite.updateSubscriber(user.email, { // un subscribed
-                type: 'unsubscribed'
-            }).then(function () {
-                user.remove(function (err) {
-                    if (!err) {
-                        res.redirect('/logout');
-                    }
-                    res.redirect('/profile');
-                })
-            }).catch(function (err) {
-                res.redirect('/profile');
-            });
+            mailerLite.removeSubscriberFromGroup(app.locals.mailer_lite_config.group, user.email)
+                .then(function () {
+                    removeUser(user, res);
+                }).catch(function (err) {
+                   removeUser(user, res);
+                });
         } else {
-            user.remove(function (err) {
-                if (!err) {
-                    res.redirect('/logout');
-                }
-                res.redirect('/profile');
-            })
+            removeUser(user, res);
         }
     });
 
@@ -336,7 +325,7 @@ module.exports = function (app, passport, nev) {
                 }).then(function () {
                     console.log("Email subscription is completed!");
                 }).catch(function (err_mailer) {
-                    console.error("Email subscription failed, error: " + err_mailer);
+                    console.error("Email subscription failed, error! ", err_mailer);
                 });
             }
 
@@ -375,4 +364,15 @@ function isSubscribed(req, res, next) {
     }
 
     res.redirect('/profile');
+}
+
+// Note: remove user
+function removeUser (user, res) {
+    user.remove(function (err) {
+        if (!err) {
+            res.redirect('/logout');
+        } else {
+            res.redirect('/profile');
+        }
+    })
 }
