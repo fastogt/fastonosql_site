bf9efcff1f72c13d13801be61329521ed773f143
topilski <atopilski@gmail.com>
2018-04-05 16:44:17 +0300

    Review login

--- config/passport.js
+++ config/passport.js
@@ -9,10 +9,14 @@ var configAuth = require('./auth'); // use this one for testing
 var KickBox = require('../app/modules/kickbox'); // use this one for testing
 
 
+function validateEmailInput(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 function validateEmail(email, done) {
     var kickBox = new KickBox();
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var is_valid = re.test(email);
+    var is_valid = validateEmailInput(email);
 
     if (!is_valid) {
         done('Invalid email input.');
@@ -37,7 +41,7 @@ function validateEmail(email, done) {
             });
             return
         }
-        cb('Can\'t resolve domain.');
+        done('Can\'t resolve domain.');
     });
 }
 
@@ -75,28 +79,27 @@ module.exports = function (nev, passport) {
         }
 
         email = email.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
-        validateEmail(email, function (err) {
+        var is_valid = validateEmailInput(email);
+        if (!is_valid) {
+            return done(null, false, req.flash('loginMessage', 'Invalid email ' + email + '.'));
+        }
+
+        User.findOne({'email': email}, function (err, user) {
+            // if there are any errors, return the error
             if (err) {
-                return done(null, false, req.flash('loginMessage', 'Invalid email ' + email + '.'));
+                return done(err);
             }
 
-            User.findOne({'email': email}, function (err, user) {
-                // if there are any errors, return the error
-                if (err) {
-                    return done(err);
-                }
+            // if no user is found, return the message
+            if (!user) {
+                return done(null, false, req.flash('loginMessage', 'No user found.'));
+            }
 
-                // if no user is found, return the message
-                if (!user) {
-                    return done(null, false, req.flash('loginMessage', 'No user found.'));
-                }
+            if (!user.validPassword(password)) {
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+            }
 
-                if (!user.validPassword(password)) {
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-                }
-
-                return done(null, user);
-            });
+            return done(null, user);
         });
     }));
 
@@ -123,11 +126,7 @@ module.exports = function (nev, passport) {
             new_user.password = new_user.generateHash(password);
             new_user.first_name = req.body.firstName.trim();
             new_user.last_name = req.body.lastName.trim();
-            if (req.body.mailSubscribe) {
-                new_user.email_subscription = true;
-            } else {
-                new_user.email_subscription = false;
-            }
+            new_user.email_subscription = req.body.mailSubscribe;
             nev.createTempUser(new_user, function (err, existingPersistentUser, newTempUser) {
                 // some sort of error
                 if (err) {
