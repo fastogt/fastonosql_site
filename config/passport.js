9f6e4152b54cc17efcc19d9bd3bb6d389d303bcf
topilski <atopilski@gmail.com>
2018-05-20 17:38:51 +0300

    Typo

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ var LocalStrategy = require('passport-local').Strategy;
 
 // load up the user model
 var User = require('../app/models/user');
-var UserBackups = require('../app/models/user_backups');
+var UsersBackup = require('../app/models/users_backup');
 var validate_email = require('../app/modules/validate_email'); // use this one for testing
 
 module.exports = function (nev, passport) {
@@ -102,13 +102,13 @@ module.exports = function (nev, passport) {
             }
             new_user.email_subscription = email_subscription;
 
-            var new_backup_user = new UserBackups();
+            var new_backup_user = new UsersBackup();
             new_backup_user.email = new_user.email;
             new_backup_user.first_name = new_user.first_name;
             new_backup_user.last_name = new_user.last_name;
             new_backup_user.email_subscription = new_user.email_subscription;
             new_backup_user.created_date = new_user.created_date;
-            UserBackups.findOneAndUpdate({'email': email}, new_backup_user, {upsert: true}, function (err, doc) {
+            UsersBackup.findOneAndUpdate({'email': email}, new_backup_user, {upsert: true}, function (err, doc) {
                 if (err) {
                     console.error(err);
                 }
