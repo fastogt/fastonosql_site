84aee4820d3fb21dc006fe5f78348988bf7c2762
topilski <atopilski@gmail.com>
2018-06-25 08:30:52 +0300

    Design

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,11 @@ var stat = {
     "active_users": 0,
     "banned_users": 0,
     "supported_users": 0,
-    "statistics": {"data": [1, 1, 1], "labels": ["Windows NT", "Mac OS X", "Linux"], "colors": ["Red", "Green", "Blue"]}
+    "statistics": {
+        "data": [1, 1, 1],
+        "labels": ["\"Windows NT\"", "\"Mac OS X\"", "\"Linux\""],
+        "colors": ["\"Red\"", "\"Green\"", "\"Blue\""]
+    }
 };
 
 scheduler.scheduleJob('* * * * *', function () {
