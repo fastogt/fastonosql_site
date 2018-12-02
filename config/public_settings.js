10f16316221b73ac3e3f42359f5706c6a5b8ccfe
topilski <atopilski@gmail.com>
2018-12-02 15:38:49 +0300

    Release up

--- config/public_settings.js
+++ config/public_settings.js
@@ -3,7 +3,7 @@ module.exports = {
     project: {
         name: 'FastoNoSQL',
         name_lowercase: 'fastonosql',
-        version: '1.23.0',
+        version: '1.23.1',
         domain: 'fastonosql.com',
         trial_days: 30,
         price_per_month: 5.99,
