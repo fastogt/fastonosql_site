8238bf35df4a277d93227d8f3e5b242fb8b92e0c
topilski <atopilski@gmail.com>
2018-12-22 09:01:32 +0300

    Price permanent

--- config/public_settings.js
+++ config/public_settings.js
@@ -8,7 +8,8 @@ module.exports = {
         trial_days: 15,
         price_per_month: 5.99,
         price_per_6_month: 32.99,
-        price_per_year: 59.99
+        price_per_year: 59.99,
+        permanent: 100
     },
 
     site: {
