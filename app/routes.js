ccb78dd8db8fce002f791b80238728ed496fab00
topilski <atopilski@gmail.com>
2019-03-24 10:07:22 +0300

    Next try

--- app/routes.js
+++ app/routes.js
@@ -781,17 +781,20 @@ module.exports = function (app, passport, nev) {
             }
 
             var emails = [];
-            users.forEach(function (user) {
-                if (user.exec_count === 0 && user.statistic.length !== 0) {
-                    user.exec_count = user.statistic.length;
-                    user.save(function (err) {
-                        if (err) {
-                            console.error('save user exec_count state error: ', err);
-                        }
-                    });
-                    emails.push(user.email);
-                }
-            });
+            (function next() {
+                users.forEach(function (user) {
+                    if (user.exec_count === 0 && user.statistic.length !== 0) {
+                        user.exec_count = user.statistic.length;
+                        user.save(function (err) {
+                            if (err) {
+                                console.error('save user exec_count state error: ', err);
+                            }
+                        });
+                        emails.push(user.email);
+                    }
+                    next();
+                });
+            })();
             res.status(200).send({emails: emails});
         });
     });
