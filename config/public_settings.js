2f9b4c7eddae430cdd45e248bf6f5f4f0807a359
topilski <atopilski@gmail.com>
2019-04-06 08:43:19 +0300

    3 days trial

--- config/public_settings.js
+++ config/public_settings.js
@@ -5,7 +5,7 @@ module.exports = {
         name_lowercase: 'fastonosql',
         version: '2.2.1',
         domain: 'fastonosql.com',
-        trial_days: 15,
+        trial_days: 3,
         price_per_month: 5.99,
         price_per_6_month: 32.99,
         price_per_year: 59.99,
